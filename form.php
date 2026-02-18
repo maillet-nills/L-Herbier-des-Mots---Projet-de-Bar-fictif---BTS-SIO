ea77daab02c258e3ec0ba49b42deb87690505020
Nills Maillet <mailletnills@gmail.com>
2026-02-18 10:05:20 +0100

    Insert form data into MySQL securely

--- form.php
+++ form.php
@@ -7,6 +7,39 @@
 </head>
 <body>
     <?php
+        ini_set('display_errors', 1);
+        error_reporting(E_ALL);
+    ?>
+
+    <?php
+        $host = "localhost";
+        $user = "root";
+        $password = "";
+        $dbname = "herbierdesmots";
+
+        $conn = new mysqli($host, $user, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Erreur de connexion : " . $conn->connect_error);
+        }
+
+        $service = intval($_POST['service-stars']);
+        $ambiance = intval($_POST['ambiance-stars']);
+        $food = intval($_POST['food-stars']);
+        $situation = $conn->real_escape_string($_POST['situation']);
+        $comment = $conn->real_escape_string($_POST['review']);
+
+        $sql = "INSERT INTO review (review_service, review_ambiance, review_food, review_situation, review_comment)
+            VALUES ('$service', '$ambiance', '$food', '$situation', '$comment')";
+        
+        if ($conn->query($sql) === TRUE) {
+            echo "Nouvel avis enregistré avec succès !<br><br>";
+        } else {
+            echo "Erreur : " . $sql . "<br>" . $conn->error;
+        }
+        
+        $conn->close();
+
         echo '<b>Service : </b>'.$_POST['service-stars'].'⭐<br>';
         echo '<b>Ambience : </b>'.$_POST['ambiance-stars'].'⭐<br>';
         echo '<b>Nourriture : </b>'.$_POST['food-stars'].'⭐<br>';
